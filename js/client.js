da23f2ea7d7217be3bce107c95ce7ad2d2f63fca
Mark McNally <mcnal1987@gmail.com>
2019-06-20 11:51:14 -0500

    Understood this, events, and we walked through adding events onto DOM

--- js/client.js
+++ js/client.js
@@ -5,7 +5,7 @@ const playfair = [
     'Sam', 'Adam', 'Danielle', 'Kyle', 'Blake',
     'Mitch', 'Jake'
 ]
-
+ // events are always going to be events.
 $(document).ready(onReady); // adds content to the page. (DOM Document Object Model)
 // .ready is a jQuery event and lets the document know that it is ready to run the function.
 // its important not to call the function like $(function onReady()); just call the name onReady. 
@@ -13,6 +13,22 @@ $(document).ready(onReady); // adds content to the page. (DOM Document Object Mo
 // Function onReady takes the information from jQuery bling and puts that onto Dom. when DOM is ready.
 function onReady() {
     renderStudentList(playfair);
+
+    // Need to select something already on the DOM.
+    // can filter the event to something new(or dynamically generated)
+    //do this with the 2nd argument to 'on' function ('.student)
+    $('#students').on('click', '.student', showAwesomeAlert); //jQuery is calling the function for you 
+    
+    //Shouldnt do this becasue it will break later!
+    //$('.student').on('click', showAwesomeAlert);
+    console.log('This:',this);
+}
+//this function will be called when we click on a student
+//It willl show on an alert saying that student is awesome!
+function showAwesomeAlert(event) {
+    console.log('Event', event);
+    console.log(this); // 'this' helps you target where the contextual event is going to. Its what generated the event.
+    //alert('Check the console...');
 }
 
 // This functionwilltake in an array of students,
@@ -27,5 +43,5 @@ function renderStudentList(studentList) { //student list will allow you to pass 
 //and append it to the '#student' id on the HTML page.
 //  this function of renderStudentList onto the DOM through onReady.  
 function renderStudent(student) {
-    $('#students').append(`<li>${student}</li>`); // makes the list of names a listed.
+    $('#students').append(`<li class="student">${student}</li>`); // makes the list of names a listed.
 }
